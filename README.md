001514e2dfcea057dbe2b89eb6ed10fa5a1bb5f6
wingmeng <wingmeng@qq.com>
2019-01-06 13:22:03 +0800

    :tada: 完成项目

--- README.md
+++ README.md
@@ -1,74 +1,28 @@
-# Project Overview
+# Feedreader
 
-In this project you are given a web-based application that reads RSS feeds. The original developer of this application clearly saw the value in testing, they've already included [Jasmine](http://jasmine.github.io/) and even started writing their first test suite! Unfortunately, they decided to move on to start their own company and we're now left with an application with an incomplete test suite. That's where you come in.
+> 一个基于 TDD（测试驱动开发） 的用来读取 rss 源的应用。采用 [Jasmine](http://jasmine.github.io) 进行测试。
 
+## 项目目的
 
-## Why this Project?
+测试是开发过程中一个很重要的部分，很多组织把一个标准的开发过程称之为测试驱动开发。意思就是开发人员在他们开始着手编写应用代码之前先写好测试用例。不管是在一个推崇测试驱动开发的组织，或者是一个编写测试只是为了防止未来的开发中出现与已有代码冲突的 bug 的团队工作，测试都是我们要掌握的一项重要技能。
 
-Testing is an important part of the development process and many organizations practice a standard of development known as "test-driven development." This is when developers write tests first, before they ever start developing their application. All the tests initially fail and then they start writing application code to make these tests pass.
+## 测试清单
 
-Whether you work in an organization that uses test-driven development or in an organization that uses tests to make sure future feature development doesn't break existing features, it's an important skill to have!
+1. Rss 源测试
+    1. allFeeds 变量是否被定义
+    2. allFeeds 对象中的源是否都有链接字段，且链接字段不为空
+    3. allFeeds 对象中的源是否都有名称字段，且名称字段不为空
 
+2. 导航菜单测试
+    1. 菜单默认隐藏
+    2. 菜单切换图标点击后菜单会切换可见状态
 
-## What will I learn?
+3. 初始内容测试
+    1. loadFeed 函数被调用
+    2. loadFeed 函数工作正常（.feed 容器下至少有1个 .entry 元素）
 
-You will learn how to use Jasmine to write a number of tests against a pre-existing application. These will test the underlying business logic of the application as well as the event handling and DOM manipulation.
+4. 接口功能测试
+    1. 切换加载 allFeeds 中的源后页面内容会随之改变
 
-
-## How will this help my career?
-
-Writing effective tests requires analyzing multiple aspects of an application including the HTML, CSS and JavaScript - an extremely important skill when changing teams or joining a new company.
-
-Good tests give you the ability to quickly analyze whether new code breaks an existing feature within your codebase, without having to manually test all of the functionality.
-
-
-# Development Strategy
-
-For a refresher (or reference) before you begin writing code, we recommend reviewing the content from [JavaScript Testing](https://www.udacity.com/course/javascript-testing--ud549). Your project will be evaluated by a Udacity code reviewer according to the [Feed Reader Testing project rubric](https://review.udacity.com/#!/rubrics/18/view). Please review for detailed project requirements.
-
-1. Familiarize yourself with the starter code
-    * Open up `index.html` and review the functionality of the application within your browser
-    * What is all the code in `app.js` doing? Be sure to read all code comments
-    * Check out `style.css`. How is styling applied to the application?
-2. Explore the Jasmine spec file in `feedreader.js`
-    * This is the file in which you'll be writing your tests
-    * Make sure to read all code comments here as well
-    * Review the [Jasmine documentation](http://jasmine.github.io) if needed
-3. Edit the `allFeeds` variable in `app.js` to make the provided test fail
-    * See how Jasmine visualizes this failure in your application
-    * Return the `allFeeds` variable to a passing state after reviewing the failed test
-4. Write a test that loops through each feed in the `allFeeds` object and ensures it has a URL defined _and_ that the URL is not empty
-    * For example, how would you use a `for...of` loop in this test?
-5. Write a test that loops through each feed in the `allFeeds` object and ensures it has a name defined and that the name is not empty
-    * Think about how you wrote the previous test. What are you testing for this time?
-6. Write a new test suite named `"The menu"`
-    * What are you `describe`-ing in this test suite?
-7. Write a test that ensures the menu element is hidden by default
-    * You'll have to analyze the HTML and the CSS to determine how the hiding/showing of the menu element is implemented
-    * What code in `app.js` is directly involved with toggling the menu on and off?
-8. Write a test that ensures the menu changes visibility when the menu icon is clicked. This test should have two expectations: does the menu display itself when clicked, and does it hide when clicked again?
-    * Think about how you wrote the previous test. What is different this time around?
-    * Which clickable element are you checking for?
-    * How do you "simulate" a mouse click that element without actually clicking it?
-9. Write a test suite named `"Initial Entries"`
-    * What are you `describe`-ing in this test suite?
-10. Write a test that ensures when the `loadFeed` function is called and completes its work, there is at least a single `.entry` element within the `.feed` container
-    * How does Jasmine's `beforeEach()`function work?
-    * How does the `loadFeed()` function in `app.js` work? Is it synchronous or asynchronous?
-11. Write a test suite named `"New Feed Selection"`
-    * What are you `describe`-ing in this test suite?
-12. Write a test that ensures when a new feed is loaded by the `loadFeed` function that the content actually changes
-    * How is this test different from the previous test?
-
-Additionally, note that:
-
- * No test should be dependent on the results of another
- * Callbacks should be used to ensure that feeds are loaded before they are tested
- * Error handling should be implemented for undefined variables and out-of-bound array access
- * When complete, all of your tests should pass
-
-When you're all finished, write a `README` file detailing all steps required to successfully run the application. If you have added additional tests, provide documentation for what these future features are and what the tests are checking for.
-
-# Contributing
-
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+5. 接口响应测试
+    1. 测试 allFeeds 中所有订阅源接口的响应时间是否不超过10s
--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,72 +1,214 @@
 /* feedreader.js
  *
- * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
+ * 这是 Jasmine 会读取的spec文件，它包含所有的要在你应用上面运行的测试。
  */
 
-/* We're placing all of our tests within the $() function,
- * since some of these tests may require DOM elements. We want
- * to ensure they don't run until the DOM is ready.
+/* 我们把所有的测试都放在了 $() 函数里面。因为有些测试需要 DOM 元素。
+ * 我们得保证在 DOM 准备好之前他们不会被运行。
  */
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+    /* 这是我们第一个测试用例 - 其中包含了一定数量的测试。这个用例的测试
+     * 都是关于 Rss 源的定义的，也就是应用中的 allFeeds 变量。
+     */
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
-        it('are defined', function() {
+        /* 这是我们的第一个测试 - 它用来保证 allFeeds 变量被定义了而且
+         * 不是空的。在你开始做这个项目剩下的工作之前最好实验一下这个测试
+         * 比如你把 app.js 里面的 allFeeds 变量变成一个空的数组然后刷新
+         * 页面看看会发生什么。
+        */
+        it('are defined and not be empty', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
+        /* TODO:
+         * 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有链接字段而且链接不是空的。
          */
+		it('each item has `url` and url not be empty', function() {
+			allFeeds.forEach(function(item) {
+				expect(item.url).toBeDefined();
+				expect(item.url).not.toMatch(/^\s*$/g);
+			});
+		});
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
+        /* TODO:
+         * 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有名字字段而且不是空的。
          */
+		it('each item has `name` and name not be empty', function() {
+			allFeeds.forEach(function(item) {
+				expect(item.name).toBeDefined();
+				expect(item.name).not.toMatch(/^\s*$/g);
+			});
+		});
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
-
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
+    /* TODO: 写一个叫做 "The menu" 的测试用例 */
+	describe('The menu', function() {
+        /* TODO:
+         * 写一个测试用例保证菜单元素默认是隐藏的。你需要分析 html 和 css
+         * 来搞清楚我们是怎么实现隐藏/展示菜单元素的。
          */
+		it('is hidden by default', function() {
+			// 'menu-hidden' 类名控制菜单元素可见状态
+			expect($('body').hasClass('menu-hidden')).toBe(true);
+		});
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+        // 触发菜单图标的点击事件  
+        var clickMenuIcon = function() {
+            $('.menu-icon-link').trigger('click');
+            return true;
+        };
 
-    /* TODO: Write a new test suite named "Initial Entries" */
-
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
+        /* 判断菜单是否隐藏（body 有无 menu-hidden 这个类）
+         * @return {Boolean} ：
+         *      1. true: 菜单是隐藏的
+         *      2. false：菜单是可见的
          */
+        var isMenuHidden = function() {
+            return $('body').hasClass('menu-hidden');
+        };
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
-
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
+        /* TODO:
+         * 写一个测试用例保证当菜单图标被点击的时候菜单会切换可见状态。这个
+         * 测试应该包含两个 expectation ：
+         * 	1. 当点击图标的时候菜单是否显示；
+         * 	2. 再次点击的时候是否隐藏。
          */
+        it('toggle visible after clicked', function() {
+            expect(clickMenuIcon()).not.toBe(isMenuHidden());  // 首次点击显示
+            expect(clickMenuIcon()).toBe(isMenuHidden());  // 再次点击隐藏
+        });
+	});
+
+    /* TODO: 写一个叫做 "Initial Entries" 的测试用例 */
+	describe('Initial Entries', function() {
+        var isBeenCalled = false;
+
+        beforeEach(function(done) {
+            // 使用 loadFeed 提供的第2个形参（回调函数）来判断异步请求是否完成
+            loadFeed(0, function() {
+                isBeenCalled = true;
+                done();
+            });
+        });
+
+        /* TODO:
+         * 写一个测试保证 loadFeed 函数被调用而且工作正常，即在 .feed 容器元素
+         * 里面至少有一个 .entry 的元素。
+         *
+         * 记住 loadFeed() 函数是异步的所以这个而是应该使用 Jasmine 的 beforeEach
+         * 和异步的 done() 函数。
+         */
+        it('function to have been called and goes well', function(done) {
+            expect(isBeenCalled).toBe(true);
+            expect($('.feed .entry').length).toBeTruthy();
+            // expect(!!$('.feed .entry').length).toBe(true);
+            done();
+        });
+	});
+
+    /* TODO: 写一个叫做 "New Feed Selection" 的测试用例 */
+    describe('New Feed Selection', function() {
+        var $title = $('.header-title');
+        var $feed = $('.feed');
+        var responses = [];
+        var originalTimeout;
+
+        beforeAll(function(done) {
+            originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+            // 设置 jasmine 超时时间（为每个订阅源接口预留20s）
+            jasmine.DEFAULT_TIMEOUT_INTERVAL = allFeeds.length * 20 * 1000;
+
+            allFeeds.forEach(function(feed, idx) {
+                loadFeed(idx, function() {
+                    // 收集请求完成后当前DOM上的内容
+                    responses.push({
+                        id: idx,
+                        title: $title.text(),
+                        content: $.trim($feed.children().eq(0).text())  // 获取第一条内容
+                    });
+
+                    // console.warn(responses.length, allFeeds.length);
+                    // 跑完所有的订阅源接口再回调
+                    if (responses.length === allFeeds.length) {
+                        done();
+                    }
+                });
+            });
+        });
+
+        afterAll(function() {
+            jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+            // console.log('END');
+        });  
+
+        // 检测数组项目是否重复
+        var hasRepeated = function(arr) {
+            var hash = {};
+            var titleElm;
+            var contentElm;
+
+            for (var i = 0; i < arr.length; i++) {
+                titleElm = arr[i].title;
+                contentElm = arr[i].content;
+
+                // 存在重复的标题或内容
+                if (hash[titleElm] || hash[contentElm]) {
+                    return true;
+                } else {
+                    hash[titleElm] = true;
+                    hash[contentElm] = true;
+                }
+            }
+
+            return false;
+        };
+
+        /* TODO:
+         * 写一个测试保证当用 loadFeed 函数加载一个新源的时候内容会真的改变。
+         * 记住，loadFeed() 函数是异步的。
+         */
+        it('content changes when a new feed is loaded', function(done) {
+            // console.log(responses);
+            // responses 为每次请求返回后当时DOM的内容信息
+            // 如 responses 中无重复项，表面每次加载新源时内容发生了改变
+            expect(hasRepeated(responses)).toBe(false);
+            done();
+        });
+    });
+    
+    /* 检测每个订阅源接口的响应时间，且时间不得超过 10s 钟 */
+    describe('Feeds Response Speed Test', function() {
+        var limit = 10;  // 要求的最大响应时间
+        var sendReq = function(feedUrl) {
+            return $.ajax({
+                type: 'POST',
+                url: 'https://rsstojson.udacity.com/parseFeed',
+                data: JSON.stringify({ url: feedUrl }),
+                contentType: 'application/json',
+            });
+        };
+
+        allFeeds.forEach(function(feed, idx) {
+            describe('"' + feed.name + '"', function() {
+                var startTime;
+
+                beforeEach(function(done) {
+                    startTime = +new Date();
+
+                    sendReq(feed.url).always(function() {
+                        done();
+                    });
+                });
+
+                it('time to be less than ' + limit + 's', function() {
+                    var time = (+new Date() - startTime) / 1000;
+                    expect(time).toBeLessThan(limit);
+                });
+            });
+        });
+    });
 }());
